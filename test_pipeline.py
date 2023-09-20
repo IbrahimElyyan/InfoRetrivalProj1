7e5ff68d3a324a6f4ccfd80a06a684fa3690d0a2
IbrahimElyyan <ibrahimelyyan17@gmail.com>
2023-09-20 17:02:11 -0400

    reader function works

--- test_pipeline.py
+++ test_pipeline.py
@@ -1,15 +1,26 @@
+import os
 import unittest
-import tokenizer, lowercaser, porter_stemmer, stopword_remover
+import reader, tokenizer, lowercaser, porter_stemmer, stopword_remover
 
 # Replace 'your_module' with the actual module where your functions are defined.
 
 class TestReutersProcessing(unittest.TestCase):
 
-    def test_read_reuters21578s(self):
-        # Add test cases for reading and extracting raw text
-        # Ensure the returned documents match the expected length and content
-        pass
+    def test_read_reuters21578(self):
+        test_path = 'test_text/'
+        os.makedirs(test_path)
+        with open(os.path.join(test_path, 'reut_test_reader.txt'), 'w') as f:
+            f.write('This is a sample text to test reader function.')
 
+        # Call the function and check the results
+        documents = reader.read_reuters21578(test_path)
+        self.assertEqual(len(documents), 1)
+        self.assertEqual(documents[0], 'This is a sample text to test reader function.')
+
+        # Clean up the mock corpus directory
+        os.remove(os.path.join(test_path, 'reut_test_reader.txt'))
+        os.rmdir(test_path)
+        
     def test_tokenize_reuters21578(self):
         # Add test cases for tokenization
         # Ensure the tokenized output matches the expected tokenized documents
